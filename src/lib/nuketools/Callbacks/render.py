70a074121ac87e03f22e228988ed1e78e964633d
Carlos Garcia <carlos@umedia.eu>
2018-04-03 14:49:59 -0700

    Fix a bug when collecting read nodes in the tree

--- src/lib/nuketools/Callbacks/render.py
+++ src/lib/nuketools/Callbacks/render.py
@@ -1,6 +1,5 @@
 """Render callbacks."""
 import nuke
-from ..App.NukeHook import NukeHook
 from fnmatch import fnmatch
 
 
@@ -11,7 +10,7 @@ __modifyMetadataName = '__addMetadataBR'
 
 def beforeRender():
     """Triggered before a render is launch."""
-    writeNode = nuke.thisNode()
+    writeNode = nuke.thisParent()
     if not hasMetadata(writeNode):
         addMetadata(writeNode)
 
@@ -23,10 +22,14 @@ def afterRender():
 
 def deleteMetadataNode():
     """Delete the node created to add the metadata if exists."""
-    metadataNode = nuke.toNode(__modifyMetadataName)
-    if not metadataNode:
+    writeNode = nuke.thisParent()
+    metaNode = writeNode.input(0)
+
+    if not metaNode.name() == __modifyMetadataName:
         return
-    nuke.delete(metadataNode)
+    cropNode = metaNode.input(0)
+    nuke.delete(metaNode)
+    writeNode.setInput(0, cropNode)
 
 
 def addMetadata(writeNode):
@@ -39,28 +42,68 @@ def addMetadata(writeNode):
     """
     xposWriteNode = writeNode.xpos()
     yposWriteNode = writeNode.ypos()
-    value = None
-    name = None
-    for readNode in NukeHook.queryAllNodes('Read'):
-        metadata = readNode.metadata()
+    values = {}
+    nodesFound = []
+    traverseCompTree(writeNode, 'read', nodesFound)
+
+    # check if the node have the information need it
+    for node in nodesFound:
+        metadata = node.metadata()
         if not metadata:
             continue
         for metadataKey in metadata.keys():
             if fnmatch(metadataKey, __metadataConvention):
-                value = metadata[metadataKey]
-                name = metadataKey.split('/')[-1]
-                break
+                metavalue = metadata[metadataKey]
+                metaname = metadataKey.split('/')[-1]
+                if metaname not in values.keys():
+                    values[metaname] = (metavalue, node.name())
 
-    if value is None or name is None:
+                elif values[metaname][0] != metavalue:
+                    raise ValueError(
+                        'Can not determine a Neutral Cdl.\n{rNodeOne} and {rNodeTwo} have different Neutral Cdl values'.format(
+                            rNodeOne=values[metaname][1],
+                            rNodeTwo=node.name()
+                        )
+                    )
+    if not values:
         return
-
     metadataNode = nuke.createNode('ModifyMetaData')
     metadataNode.setName(__modifyMetadataName)
     metadataNode.setXYpos(xposWriteNode, yposWriteNode - 40)
-    metadataNode.knob('metadata').fromScript('{' + "set {name}".format(name=name) + " \"{}\"".format(value.replace('"', '\\"')) + '}')
+    for name, value in values.iteritems():
+        metadataNode.knob('metadata').fromScript('{' + "set {name}".format(name=name) + " \"{}\"".format(value[0].replace('"', '\\"')) + '}')
     connectedNode = writeNode.input(0)
     metadataNode.setInput(0, connectedNode)
     writeNode.setInput(0, metadataNode)
+
+
+def traverseCompTree(node, nodeType, nodesFound, nodeCache=[]):
+    """
+    Traverse the nuke node tree bottom to top from the a specific node.
+
+    :param node: The node to start the recursion from.
+    :type node: nuke.Node
+    :param nodeType: The type of the node to be found in the node tree.
+    :type nodeType: str
+    :param nodesFound: The list to fill with the nodes found.
+    :type nodesFound: list
+    :param nodeCache: A list of nodes that has been revised.
+    :type nodeCache: list
+    """
+    # Create a cache for nodes that have been process
+    if node in nodeCache:
+        return
+
+    nodeCache.append(node)
+
+    for i in range(node.inputs()):
+        inputNode = node.input(i)
+        if inputNode is None:
+            continue
+        if inputNode.Class() == nodeType.capitalize():
+            nodesFound.append(inputNode)
+        print 'NodesFound --->>>>>>> ', nodesFound
+        traverseCompTree(inputNode, nodeType, nodesFound, nodeCache)
 
 
 def hasMetadata(writeNode):
